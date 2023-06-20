ab58d0a28ae76343b79c60f7ece75864273e5fc7
luluwambua <luluwambua@gmail.com>
2023-06-20 21:06:36 +0300

    Create entries.py
    A very simple implementation of a front end page for submitting entries into a diary app with a HTML front end and Flask backend.

---
+++ entries.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template_string, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def entries():
+    return render_template_string('''
+    <html>
+        <body>
+            <h3>entries</h3>
+            <input type="text" placeholder="day"/>
+            </br>
+            <br>
+            <input type="time" placeholder="time"/>
+            </br>
+            <br>
+            <input type="text" placeholder="entry"/>
+            </br>
+            <br>
+            <input type="submit" placeholder="submit" />
+            </br>
+
+        </body>
+    </html>
+    ''')
+
+if __name__ == "__main__":
+    app.run()
