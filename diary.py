ecd7e8f9573fd9859b66d3a2edf6d159256ef5e6
luluwambua <luluwambua@gmail.com>
2023-06-28 11:30:48 +0300

    Update diary.py
    added form and route

--- diary.py
+++ diary.py
@@ -14,9 +14,40 @@ def homepage():
     <html>
         <head></head>
         <body>
-        <h3>homepage</h3>
-        </body>
+            <form action="/submit" method="post">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name"><br><br>
+                <label for="email">Email:</label>
+                <input type="text" id="email" name="email"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+   
     </html>
+    ''')
+@app.route('/upload', methods = ['GET','POST'])
+def submit():
+    if request.method == 'POST':
+	    SCHEDULE = request.form['SCHEDULE']
+                
+        with sqlite3.connect("database.db") as users:
+		    cursor = users.cursor()
+            cursor.execute("INSERT INTO diary \
+			(SCHEDULE) VALUES (?)",
+						(SCHEDULE))
+	users.commit()
+	return render_template_string('''
+	<!DOCTYPE html>
+<html>
+	<head>
+		<title>Flask and SQLite </title>
+	</head>
+	<body>
+		<h1>Build Web App Using Flask and SQLite</h1>
+		<button class="btn" type="button" onclick="window.location.href='{{ url_for('join') }}';">Fill form to get updates</button><br/>
+		<button class="btn" type="button" onclick="window.location.href='{{ url_for('participants') }}';">Check participant list</button>
+	</body>
+</html>
+
     ''')
 
 if __name__ == "__main__":
