41ae87835c1b70b9b650e81319ed9967593dc06e
luluwambua <luluwambua@gmail.com>
2023-07-02 10:09:36 +0300

    Update diary.py

--- diary.py
+++ diary.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template_string, request
 import sqlite3
 import jinja2
+import os
 
 connection = sqlite3.connect('diary.db')
 cursor = connection.cursor()
@@ -14,41 +15,22 @@ def homepage():
     <html>
         <head></head>
         <body>
-            <form action="/submit" method="post">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name"><br><br>
-                <label for="email">Email:</label>
-                <input type="text" id="email" name="email"><br><br>
+            <form action="/upload" method="post">
+                <label for="SCHEDULE">SCHEDULE:</label>
+                <input type="text" id="SCHEDULE" name="SCHEDULE"><br><br>
                 <input type="submit" value="Submit">
             </form>
    
     </html>
     ''')
 @app.route('/upload', methods = ['GET','POST'])
-def submit():
+def upload():
     if request.method == 'POST':
 	    SCHEDULE = request.form['SCHEDULE']
-                
-        with sqlite3.connect("database.db") as users:
-		    cursor = users.cursor()
-            cursor.execute("INSERT INTO diary \
-			(SCHEDULE) VALUES (?)",
-						(SCHEDULE))
-	users.commit()
-	return render_template_string('''
-	<!DOCTYPE html>
-<html>
-	<head>
-		<title>Flask and SQLite </title>
-	</head>
-	<body>
-		<h1>Build Web App Using Flask and SQLite</h1>
-		<button class="btn" type="button" onclick="window.location.href='{{ url_for('join') }}';">Fill form to get updates</button><br/>
-		<button class="btn" type="button" onclick="window.location.href='{{ url_for('participants') }}';">Check participant list</button>
-	</body>
-</html>
-
-    ''')
+with sqlite3.connect('diary.db') as connection:
+     cursor = connection.cursor()
+     cursor.execute('INSERT INTO DIARY(SCHEDULE) VALUES (?)',(SCHEDULE))
+     connection.commit()
 
 if __name__ == "__main__":
     app.run()
