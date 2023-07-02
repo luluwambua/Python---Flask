2f082a47df8d7ab3477a1bf52b4676599ec85794
luluwambua <luluwambua@gmail.com>
2023-07-02 11:47:38 +0300

    Update entries.py
    added basic HTML functionality and database tables

--- entries.py
+++ entries.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template_string, request, redirect
+from flask import Flask, render_template_string, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
-def entries():
+def index():
+    connection = sqlite3.connect('entries.db')
+    connection.execute('CREATE TABLE IF NOT EXISTS ENTRIES(day TEXT, activity TEXT)')
+    return render_template_string('''
+    <html>
+    <body>
+        <form method="post">
+            <label for="day">day:</label>
+            <input type="text" id="day" name="day">
+            <label for="activity">activity:</label>
+            <input type="text" id="activity" name="activity">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+    </html>
+    ''')
+@app.route('/upload', methods=['GET, POST'])
+def upload():
+    if request.method == 'POST':
+        day = request.method['day']
+        activity = request.method['activity']
+        with sqlite3.connect('entries.db') as users:
+            cursor = users.cursor()
+            cursor.execute('INSERT INTO ENTRIES(day,activity)VALUES(?,?)'(day, activity))
+            users.close()
     return render_template_string('''
     <html>
         <body>
-            <h3>entries</h3>
-            <input type="text" placeholder="day"/>
-            </br>
-            <br>
-            <input type="time" placeholder="time"/>
-            </br>
-            <br>
-            <input type="text" placeholder="entry"/>
-            </br>
-            <br>
-            <input type="submit" placeholder="submit" />
-            </br>
+            <h3>upload</h3>
         </body>
     </html>
     ''')
