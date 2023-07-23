eb432686e2a07d711700015e1f5dd277907b4248
luluwambua <luluwambua@gmail.com>
2023-07-23 22:48:25 +0300

    Update example.py
    created HTML page, python list and business logic

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, url_for
 
 app = Flask(__name__)
 
@@ -8,12 +8,38 @@ def index():
     <html>
     <Head></Head>
     <body>
-        <h3>welcome to the website</h3>
-        <br>
-        <a href="/login">login</a>
-        </br>
+        <form method = 'POST' action = '{{ url_for('login')}}'>
+            <input type = 'text' name = 'name' placeholder = 'name'><br>
+            <input type = 'password' name = 'password' placeholder = 'password'><br>
+            <input type = 'submit' value = 'login'>
+        </form>
     </body>
     </html>''')
+
+database = {'lulu':'lulu123',
+            'smith':'smith123',
+            'anna':'anna123'}
+
+@app.route('/login', methods = ['POST','GET'])
+def login():
+    name = request.form['name']
+    password = request.form['password']
+    if name not in database:
+        return render_template_string('''
+        <html>
+            <body>
+                <h3>access denied!<h3>
+            </body>
+        </html>
+        ''')
+    else:
+        return render_template_string('''
+        <html>
+            <body>
+                <h3>lulu</h3>
+            </body>
+        </html>
+        ''')
 
 if __name__ == "__main__":
     app.run(debug=True)
