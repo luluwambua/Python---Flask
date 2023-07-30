e733c0466aff05038097deee5b9b6a5daaa67d1c
luluwambua <luluwambua@gmail.com>
2023-07-30 19:10:47 +0300

    frontend
    removed file, added pages and functionality to the HTML pages

--- example.py
+++ example.py
@@ -30,14 +30,15 @@ def index():
         </li>
       </ul>
       <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Login</button>
       </form>
     </div>
   </div>
 </nav>
         <form method = 'POST' action = '{{ url_for('login')}}'>
+            <center><label>name</label></center>
             <center><input type = 'text' name = 'name' placeholder = 'name'></center><br>
+            <center><label>password</label></center>
             <center><input type = 'password' name = 'password' placeholder = 'password'></center><br>
             <center><input type = 'submit' value = 'login'></center>
         </form>
@@ -63,7 +64,34 @@ def login():
     else:
         return render_template_string('''
         <html>
+          <head>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+          </head>
             <body>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="/">Hidden brand</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+        <button class="btn btn-outline-success" type="submit">logout</button>
+      </form>
+    </div>
+  </div>
+</nav>
                 <center><h3>lulu</h3></center>
             </body>
         </html>
