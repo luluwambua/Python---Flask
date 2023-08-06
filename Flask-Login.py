fa7adc21871634ec229ca8286422735195003cba
luluwambua <luluwambua@gmail.com>
2023-08-06 21:15:48 +0300

    Update Flask-Login.py
    made alterations to the navbar

--- Flask-Login.py
+++ Flask-Login.py
@@ -16,13 +16,12 @@ def homepage():
     </head>
     <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-        <nav class="navbar bg-body-tertiary">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="/">
-                <img src="/static/FB_IMG_1689453837079.jpg" alt="Logo" width="60" height="60" class="d-inline-block align-text-middle">
-                taxslayer
-              </a>
-                <li class="products & pricing">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/static/FB_IMG_1689453837079.jpg" alt="Bootstrap" width="30" height="24">
+    </a>
+        <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown link
           </a>
@@ -32,8 +31,10 @@ def homepage():
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-            </div>
-          </nav>
+      </ul>
+    </div>
+  </div>
+</nav>
     </body>
 </html>
     ''')
