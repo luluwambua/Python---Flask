35b701233a191b7099875eed84f7a415bf59e961
luluwambua <luluwambua@gmail.com>
2023-08-08 09:57:11 +0300

    Update Flask-Login.py
    updated bootstrap components to work with backend, tested the register function,

--- Flask-Login.py
+++ Flask-Login.py
@@ -18,42 +18,40 @@ def homepage():
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
       <img src="/static/OIP.jpeg" alt="Bootstrap" width="150" height="30">
-    <a class="navbar-brand" href="#"></a>
+    <a class="navbar-brand" href="/"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="/">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="/">Pricing</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Blog</a>
+          <a class="nav-link" href="/register">Blog</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Calculators
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">VAT Calculator</a></li>
-            <li><a class="dropdown-item" href="#">PAYE calculator</a></li>
-            <li><a class="dropdown-item" href="#">CIT Calculator</a></li>
+            <li><a class="dropdown-item" href="/">VAT Calculator</a></li>
+            <li><a class="dropdown-item" href="/">PAYE calculator</a></li>
+            <li><a class="dropdown-item" href="/">CIT Calculator</a></li>
         </li>
       </ul>
         <li>
-        <form class="d-flex" role="search">
-        <a href="/login" class="btn" role="button" data-bs-toggle="button">Login</a>
+        <a href="/" class="btn" role="button" data-bs-toggle="button">Login</a>
         <li>
-        <form class="d-flex" role="search">
-        <a href="/register" class="btn" role="button" data-bs-toggle="button">Register</a>
+        <a href="/" class="btn btn-primary disabled" tabindex="-1" role="button" aria-disabled="False">register</a>
       </form>
         </li>
     </div>
