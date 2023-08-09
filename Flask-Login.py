0409d9e99ccdf455ad5ba115d586c738014680d1
luluwambua <luluwambua@gmail.com>
2023-08-09 21:02:16 +0300

    scripts
    attempted to add toast and related javascript

--- Flask-Login.py
+++ Flask-Login.py
@@ -260,6 +260,28 @@ def register():
       <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
         <center>
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    <img src="/static/OIP.jpeg" class="rounded me-2" alt="...">
+    <strong class="me-auto">Bootstrap</strong>
+    <small class="text-body-secondary">11 mins ago</small>
+    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+  </div>
+  <script>
+    const toastTrigger = document.getElementById('Submit')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+  </script>
+  <div class="toast-body">
+    Hello, world! This is a toast message.
+  </div>
+</div>
         <form method="post">
             <input type="text" id="name" name="name" placeholder = "name">
             <br>
